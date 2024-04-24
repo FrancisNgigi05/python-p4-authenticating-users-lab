0b0f57d62d0e8b6e8645f0b03c17ea423aaefbd3
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-04-24 11:02:54 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -47,36 +47,44 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
-    
-class Login(Resource):
-    def post(self):
-        user = User.query.filter(User.username == request.get_json()['username']).first()
 
-        session['user_id'] = user.id
-
-        return user.to_dict(), 200
-    
-class Logout(Resource):
-    def delete(self):
-        session['user_id'] = None
-        return {}, 204
-
-class CheckSession(Resource):
-    def get(self):
-        user_id = session.get('user_id')
-        if user_id:
-            user = User.query.filter(User.id == user_id).first()
-            return user.to_dict(), 200
-        
-        return {}, 401
-
-api.add_resource(CheckSession, '/check_session')
-api.add_resource(Logout, '/logout')
-api.add_resource(Login, '/login')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+
+# The login secion of the API
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        # Confirming that the user is in the db
+        user = User.query.filter(User.username == data['username']).first()
+        # Giving the specific user the session
+        session['user_id'] = user.id
+        # Returning the users data after logging in
+        return user.to_dict(), 200
+
+# The logging section of the API    
+class Logout(Resource):
+    def delete(self): 
+        # Removing the session that was assigned to a specific user
+        session['user_id'] = None
+        # Returning the logging out response to indicate the logging out is successfull
+        return {}, 204
+
+# This is the API section that makes sure that the user is logged in even after refreshing the page
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        # Checking if that specific user was assigned a session
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {}, 401
+
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
+api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
